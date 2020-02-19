86347777f626b5c26cce06fbc620695bfd6babfa
rakhirnair9 <rakhi.mysmar@gmail.com>
2020-02-19 23:20:44 +0400

    Updates

---
+++ precache-manifest.b7f64d92b15a2e25bd082cb79a2b211a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313a6189dc9750ba3e5e1c600c88dd8d",
+    "url": "/FrontEnd/index.html"
+  },
+  {
+    "revision": "20b7bbc896b0f9825566",
+    "url": "/FrontEnd/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "bb5620f204623577b9c6",
+    "url": "/FrontEnd/static/css/main.ba4356da.chunk.css"
+  },
+  {
+    "revision": "20b7bbc896b0f9825566",
+    "url": "/FrontEnd/static/js/2.d60d43ab.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/FrontEnd/static/js/2.d60d43ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb5620f204623577b9c6",
+    "url": "/FrontEnd/static/js/main.00e316a8.chunk.js"
+  },
+  {
+    "revision": "d8266a72162d94f05f4a",
+    "url": "/FrontEnd/static/js/runtime-main.5bab21e0.js"
+  }
+]);
